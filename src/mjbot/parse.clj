e62484e3dce64f0bace361e362a5c54d306286b1
MJB <max@bechtel.me.uk>
2014-10-29 21:32:25 +0000

    replace nth with get, refactor later

--- src/mjbot/parse.clj
+++ src/mjbot/parse.clj
@@ -32,35 +32,35 @@
 (defn handle-win [room smsg]
   (send-msg room "Good Game")
  	(send-msg "" (str "/leave " room))
-  (update-score (if (= (nth smsg 2) config/user) true false))
+  (update-score (if (= (get smsg 2) config/user) true false))
   (reset-state)
   (println (str "Score so far this session: " @wins "/" @losses))
   (if @config/search-more? (send-msg "" (find-battle)) (System/exit 0)))
 
 (defn get-poke-from-switch [data]
-  (nth (string/split data #",") 0))
+  (get (string/split data #",") 0))
 
 (defn parse-line [room msg]
   (if-not (or (= msg "") (= msg "|"))
     (if (= "|" (subs msg 0 1))
       (let [smsg (string/split msg #"\|")
-            type (nth smsg 1)]
+            type (get smsg 1)]
         (cond
           (= type "challstr")
-          	(login (nth smsg 2) (nth smsg 3))
+          	(login (get smsg 2) (get smsg 3))
           (= type "request")
-          	(send-msg room (play-turn (parse-json (nth smsg 2))))
+          	(send-msg room (play-turn (parse-json (get smsg 2))))
           (= type "init")
-          	(if (= (nth smsg 2) "battle") (send-msg room "Good Luck and Have Fun"))
+          	(if (= (get smsg 2) "battle") (send-msg room "Good Luck and Have Fun"))
           (= type "win")
           	(handle-win room smsg)
           (= type "updateuser")
-          	(if (= (nth smsg 2) config/user) (send-msg "" (find-battle)))
+          	(if (= (get smsg 2) config/user) (send-msg "" (find-battle)))
           (= type "player")
-          	(if (>= (count smsg) 4) (if (= (nth smsg 3) config/user) (set-who-am-i (nth smsg 2))))
+          	(if (>= (count smsg) 4) (if (= (get smsg 3) config/user) (set-who-am-i (get smsg 2))))
            ;if its a switch message, check if its the opponent
-          (and (= type "switch") (not (= (subs (nth smsg 2) 0 2) @who-am-i))) ; this is ugly as hell
-          	(reset! opp-poke (get-poke-from-switch (nth smsg 3))))))))
+          (and (= type "switch") (not (= (subs (get smsg 2) 0 2) @who-am-i))) ; this is ugly as hell
+          	(reset! opp-poke (get-poke-from-switch (get smsg 3))))))))
 
 (defn parse-msg [msg]
   (if @config/debugging? (prn msg))
